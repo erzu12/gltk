5dede97f95631607552c6c9c5a63a2acaf48a6d7
Jonas Fink <jonas.fink@stud.hslu.ch>
2026-04-15 17:01:54 +0200

    fix ci

--- run.sh
+++ run.sh
@@ -11,11 +11,11 @@ cmake --build build --config Debug -j 16
 
 printf "\n\n=============================================================================\n\n"
 
-# cd build/test && ctest -j 16 --output-on-failure
-# cd ../..
-#
-# if [ "$1" == "dbg" ]; then
-#     WAYLAND_DEBUG=1 ./build/gltk_test_app 2>&1 | tee gltk_wl_debug.log
-#     exit 0
-# fi
+cd build/test && ctest -j 16 --output-on-failure
+cd ../..
+
+if [ "$1" == "dbg" ]; then
+    WAYLAND_DEBUG=1 ./build/gltk_test_app 2>&1 | tee gltk_wl_debug.log
+    exit 0
+fi
 ./build/gltk_test_app
